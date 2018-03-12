40623ce6716223bb6de9da987ee43d068ef32cb6
zhubo <zhubo@58qf.com>
2018-03-12 10:13:17 +0800

    cglib代理提交

--- src/main/java/com/design/proxy/cglib_proxy/CglibProxyMain.java
+++ src/main/java/com/design/proxy/cglib_proxy/CglibProxyMain.java
@@ -11,10 +11,13 @@ public class CglibProxyMain {
 
         UserDao dao = new UserDao();
         ProxyFactory proxy = new ProxyFactory();
-
+        //利用Enhancer类生成代理类
         Enhancer enhancer = new Enhancer();
+        //设置代理类的父类
         enhancer.setSuperclass(dao.getClass());
+        //设置回调
         enhancer.setCallback(proxy);
+        //穿件代理类
         Object o = enhancer.create();
         System.out.println(o.getClass());
         UserDao proxyObj = (UserDao)o;
