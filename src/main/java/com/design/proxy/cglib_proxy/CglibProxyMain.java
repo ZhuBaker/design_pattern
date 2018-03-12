e362e70ddcd3c0743e97e20a8383af8f46d25e1d
zhubo <zhubo@58qf.com>
2018-03-12 10:10:40 +0800

    cglib代理提交

--- src/main/java/com/design/proxy/cglib_proxy/CglibProxyMain.java
+++ src/main/java/com/design/proxy/cglib_proxy/CglibProxyMain.java
@@ -1,15 +1,24 @@
 package com.design.proxy.cglib_proxy;
 
+import net.sf.cglib.proxy.Enhancer;
+
 /**
  * Created by zhubo on 2018/3/11.
  */
 public class CglibProxyMain {
 
     public static void main(String[] args) {
+
         UserDao dao = new UserDao();
-        ProxyFactory proxy = new ProxyFactory(dao);
-        UserDao proxyInstance = (UserDao) proxy.getProxyInstance();
-        System.out.println(proxyInstance.getClass());
-        proxyInstance.save();
+        ProxyFactory proxy = new ProxyFactory();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(dao.getClass());
+        enhancer.setCallback(proxy);
+        Object o = enhancer.create();
+        System.out.println(o.getClass());
+        UserDao proxyObj = (UserDao)o;
+        proxyObj.save();
+
     }
 }
