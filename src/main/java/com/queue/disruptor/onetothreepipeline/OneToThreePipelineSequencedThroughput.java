fafd387f636c8e58c5d3c67bc0b3afd4ba3e7dee
zhubo <670403684@qq.com>
2018-04-18 21:55:59 +0800

    disruptor OneToOne

--- src/main/java/com/queue/disruptor/onetothreepipeline/OneToThreePipelineSequencedThroughput.java
+++ src/main/java/com/queue/disruptor/onetothreepipeline/OneToThreePipelineSequencedThroughput.java
@@ -1,12 +1,18 @@
 package com.queue.disruptor.onetothreepipeline;
 
+import com.lmax.disruptor.BatchEventProcessor;
 import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.SequenceBarrier;
 import com.lmax.disruptor.YieldingWaitStrategy;
 import com.queue.disruptor.AbstractPerfTestDisruptor;
 import com.queue.disruptor.DaemonThreadFactory;
+import com.queue.disruptor.PerfTestUtil;
 import com.queue.disruptor.onetoone.PerfTestContext;
 import com.queue.disruptor.support.FunctionEvent;
+import com.queue.disruptor.support.FunctionEventHandler;
+import com.queue.disruptor.support.FunctionStep;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -43,15 +49,65 @@ public class OneToThreePipelineSequencedThroughput extends AbstractPerfTestDisru
 
     private final RingBuffer<FunctionEvent> ringBuffer =
             createSingleProducer(FunctionEvent.EVENT_FACTORY, BUFFER_SIZE, new YieldingWaitStrategy());
+    private final SequenceBarrier stepOneSequenceBarrier = ringBuffer.newBarrier();
+    private final FunctionEventHandler stepOneFunctionHandler = new FunctionEventHandler(FunctionStep.ONE);
+    BatchEventProcessor<FunctionEvent> stepOneBatchProcessor = new BatchEventProcessor<>(ringBuffer,stepOneSequenceBarrier,stepOneFunctionHandler);
+
+    private final SequenceBarrier stepTwoSequenceBarrier = ringBuffer.newBarrier(stepOneBatchProcessor.getSequence());
+    private final FunctionEventHandler stepTwoFunctionHandler = new FunctionEventHandler(FunctionStep.TWO);
+    private final BatchEventProcessor<FunctionEvent> stepTwoBatchProcessor =
+            new BatchEventProcessor<FunctionEvent>(ringBuffer, stepTwoSequenceBarrier, stepTwoFunctionHandler);
+
+    private final SequenceBarrier stepThreeSequenceBarrier = ringBuffer.newBarrier(stepTwoBatchProcessor.getSequence());
+    private final FunctionEventHandler stepThreeFunctionHandler = new FunctionEventHandler(FunctionStep.THREE);
+    private final BatchEventProcessor<FunctionEvent> stepThreeBatchProcessor =
+            new BatchEventProcessor<FunctionEvent>(ringBuffer, stepThreeSequenceBarrier, stepThreeFunctionHandler);
+    // 构造反向依赖，stepThreeBatchProcessor的Sequence最小
+    {
+        ringBuffer.addGatingSequences(stepThreeBatchProcessor.getSequence());
+    }
 
 
     @Override
     protected int getRequiredProcessorCount() {
-        return 0;
+        return 4;
     }
 
     @Override
     protected PerfTestContext runDisruptorPass() throws Exception {
-        return null;
+        PerfTestContext perfTestContext = new PerfTestContext();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        stepThreeFunctionHandler.reset(latch, stepThreeBatchProcessor.getSequence().get() + ITERATIONS);
+        executor.submit(stepOneBatchProcessor);
+        executor.submit(stepTwoBatchProcessor);
+        executor.submit(stepThreeBatchProcessor);
+
+        long start = System.currentTimeMillis();
+
+        long operandTwo = OPERAND_TWO_INITIAL_VALUE;
+        for (long i = 0; i < ITERATIONS; i++)
+        {
+            long sequence = ringBuffer.next();
+            FunctionEvent event = ringBuffer.get(sequence);
+            event.setOperandOne(i);
+            event.setOperandTwo(operandTwo--);
+            ringBuffer.publish(sequence);
+        }
+
+        latch.await();
+        perfTestContext.setDisruptorOps((ITERATIONS * 1000L) / (System.currentTimeMillis() - start));
+
+        stepOneBatchProcessor.halt();
+        stepTwoBatchProcessor.halt();
+        stepThreeBatchProcessor.halt();
+
+        PerfTestUtil.failIfNot(expectedResult, stepThreeFunctionHandler.getStepThreeCounter());
+
+        return perfTestContext;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new OneToThreePipelineSequencedThroughput().testImplementations();
     }
 }
